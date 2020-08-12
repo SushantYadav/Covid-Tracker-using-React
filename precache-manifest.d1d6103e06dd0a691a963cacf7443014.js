19a185d6849261a2915d43f5afafcd5d8483004f
Sushant Yadav <sushanty@techepoch.com>
2020-08-12 16:17:00 +0530

    Updates

---
+++ precache-manifest.d1d6103e06dd0a691a963cacf7443014.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c878845fc27114fbad15f9baa917c81",
+    "url": "/Covid-Tracker-using-React/index.html"
+  },
+  {
+    "revision": "42c84bb88b308d87943f",
+    "url": "/Covid-Tracker-using-React/static/css/main.4f92920c.chunk.css"
+  },
+  {
+    "revision": "b02b1ae80f801eada12f",
+    "url": "/Covid-Tracker-using-React/static/js/2.06fe8f82.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/Covid-Tracker-using-React/static/js/2.06fe8f82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42c84bb88b308d87943f",
+    "url": "/Covid-Tracker-using-React/static/js/main.d1b294f8.chunk.js"
+  },
+  {
+    "revision": "ecc461f3a41d27c2c421",
+    "url": "/Covid-Tracker-using-React/static/js/runtime-main.b7897b51.js"
+  }
+]);
